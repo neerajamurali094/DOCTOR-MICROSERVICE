a3a8af696336e0dc4bbc1aed947ce9cd97ac8c25
mayabytatech <maya.k.k@lxisoft.com>
2019-05-01 16:08:54 +0530

    autowired added

--- src/main/java/com/bytatech/ayoos/web/rest/DoctorResource.java
+++ src/main/java/com/bytatech/ayoos/web/rest/DoctorResource.java
@@ -10,6 +10,7 @@ import com.bytatech.ayoos.service.mapper.DoctorMapper;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
@@ -39,6 +40,7 @@ public class DoctorResource {
 
     private final DoctorService doctorService;
     
+    @Autowired
     private DoctorMapper doctorMapper;
     
     public DoctorResource(DoctorService doctorService) {
